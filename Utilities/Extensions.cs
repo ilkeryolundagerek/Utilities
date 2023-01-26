057d6a974a9fb23fc73bad27b29daad47b33ea54
cemdos <cem.dos@hotmail.com>
2023-01-26 22:00:26 +0300

    26.01.2023 22:00

---
+++ Utilities/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class Extensions
+    {
+        public static string ToUrl(this string text)
+        {
+            string result = text.ToLower().Trim();
+            result = result.Replace("ş", "s")
+                           .Replace("ı", "i")
+                           .Replace("ğ", "g")
+                           .Replace("ç", "c")
+                           .Replace("ö", "o")
+                           .Replace("ü", "u");
+            result = Regex.Replace(result, @"[^\w\s]", "");
+            result = Regex.Replace(result, @"\s+", " ").Trim();
+            return result.Replace(" ", "-");
+        }
+    }
+}
---
+++ Utilities/Pagination.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class Pagination<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int StartPage { get; set; }
+        public int EndPage { get; set; }
+        public IEnumerable<T> Items { get; set; }
+
+        public Pagination(IEnumerable<T> all_items, int pagesize, int currentPage = 1, int pageLimit = 0)
+        {
+            CurrentPage = currentPage;
+            PageSize = pagesize;
+            TotalPages = all_items.Count() % pagesize > 0 ? all_items.Count() / pagesize + 1 : all_items.Count() / pagesize;
+            if (pageLimit > 0)
+            {
+                StartPage = currentPage - pageLimit <= 1 ? 1 : currentPage - pageLimit;
+                EndPage = currentPage + pageLimit >= TotalPages ? TotalPages : currentPage + pageLimit;
+            }
+            else
+            {
+                StartPage = 1;
+                EndPage = TotalPages;
+            }
+            Items = all_items.Skip((currentPage - 1) * pagesize).Take(pagesize).ToList();
+        }
+    }
+}
